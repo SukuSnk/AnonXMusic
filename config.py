c54355942264c1b32d68741f2351936cafd375f2
SukuSnk <136146956+SukuSnk@users.noreply.github.com>
2023-06-10 11:59:12 +0530

    Update config.py
    Signed-off-by: SukuSnk <136146956+SukuSnk@users.noreply.github.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/a75dc6811d7b8b918091c.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
